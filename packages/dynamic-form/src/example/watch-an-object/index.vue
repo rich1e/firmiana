43a8da9bc8732ea0bb26d4016deade54321f35bf
rich1e <yuqigong@outlook.com>
2023-07-10 09:50:11 +0800

    feat: add watch-an-object

---
+++ packages/dynamic-form/src/example/watch-an-object/index.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author       : yuqigong@outlook.com
+ * @Date         : 2023-06-20 15:31:26
+ * @LastEditors  : yuqigong@outlook.com
+ * @LastEditTime : 2023-06-20 15:44:05
+-->
+<script setup lang="ts">
+  import onChange from 'on-change';
+
+  const msg = 'Watch an Object';
+
+  const user = {
+    firstName: 'Chris',
+    lastName: 'Bongers',
+    age: 10,
+  };
+
+  const watched = onChange(user, (path, value, previousValue, applyData) => {
+    console.log(path, value, previousValue, applyData);
+  });
+
+  watched.age = 15;
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
